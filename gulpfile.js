aa7825a01f16afd2774b5860834758b3e88a1b59
Tyler Audette <tyler.audette@markit.com>
2016-07-12 11:31:54 -0600

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task('html', function() {
 
 gulp.task('js', function() {
 	browserify(config.paths.mainJs)
-		.transform(reactify)
+		.transform(reactify) 
 		.bundle()
 		.on('error', console.error.bind(console))
 		.pipe(source('bundle.js'))
---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h1>About</h1>
+				<p>
+					This application uses a bunch of tech
+				</p>
+			</div>
+		);
+	}
+});
+
+module.exports = About;
